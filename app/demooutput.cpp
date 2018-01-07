0f2d3a09465ae68c0523ee8134e4f2aef5576816
Daniel Meltzer <dmeltzer.devel@gmail.com>
2018-01-07 12:54:43 -0500

    Refactor math to reference points to an origin.

--- app/demooutput.cpp
+++ app/demooutput.cpp
@@ -41,20 +41,45 @@ DemoOutput::output(const QStringList &source, const QStringList &target)
         qDebug() << "Output not enabled, aborting";
         return;
     }
+
+    // Okay, so lets take this step by step.
+    // Three points:
+    // Point of The light source itself.  This will be shifted to 0,0,0
+    // "Home" Point for the light.  This will start as 0,0,0, but be shifted by the z of the light. Immediately below the point of the light
+    // Target we are pointing at.
     QVector3D sourcePoint(source[0].toFloat(), source[1].toFloat(), source[2].toFloat());
+    QVector3D calibrationPoint(source[0].toFloat(), source[1].toFloat(), 0);
     QVector3D targetPoint(target[0].toFloat(), target[1].toFloat(), target[2].toFloat());
 
-    QVector3D calculatedVector = targetPoint - sourcePoint;
+    qDebug() << "Source: " << sourcePoint << " Origin: " << calibrationPoint << " Target: " << targetPoint;
 
-    PolarPoint dest(calculatedVector);
-    setPan(dest.pan());
-    const QString panCmd = QString("/eos/user/0/chan/%1/param/pan=%2").arg(m_channel).arg(dest.pan());
+    // We need to shift the source to 0.  This means we should subtract the source from each vector.
+    QVector3D translatedSource = sourcePoint - sourcePoint;
+    QVector3D translatedOrigin = calibrationPoint - sourcePoint;
+    QVector3D translatedTarget = targetPoint - sourcePoint;
+
+    qDebug() << "Translated: ";
+    qDebug() << "Source: " << translatedSource << " Origin: " << translatedOrigin << " Target: " << translatedTarget;
+
+    // Now lets polarize the calibartion point -- And calculate the pan/tilt to it.  This should be 0,0 if things are working.
+    PolarPoint p_Origin(translatedOrigin);
+    qDebug() << "ORIGIN: Pan: " << p_Origin.pan() << "Tilt: " << p_Origin.tilt();
+
+
+    // Polarize the target point and calculate source between the two.
+    PolarPoint p_translated(translatedTarget);
+
+    // Todo, separate a point and a vector into separate classes.
+    PolarPoint translatedVector = p_Origin.toPoint(p_translated);
+    qDebug() << "TARGET: Pan: " << translatedVector.pan() << "Tilt: " << translatedVector.tilt();
+
+    setPan(translatedVector.pan());
+    const QString panCmd = QString("/eos/user/0/chan/%1/param/pan=%2").arg(m_channel).arg(translatedVector.pan());
     m_manager->sendMessage(panCmd);
 
-    const QString tiltCmd = QString("/eos/user/0/chan/%1/param/tilt=%2").arg(m_channel).arg(dest.tilt());
+    const QString tiltCmd = QString("/eos/user/0/chan/%1/param/tilt=%2").arg(m_channel).arg(translatedVector.tilt());
     m_manager->sendMessage(tiltCmd);
-    setTilt(dest.tilt());
-    qDebug() << "Pan: " << dest.pan() << "Tilt: " << dest.tilt();
+    setTilt(translatedVector.tilt());
 }
 
 void
